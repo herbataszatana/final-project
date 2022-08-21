6a9d0279d9e423da4403c592692c2886cca23355
Anna <anma.swiatkowska@gmail.com>
2022-08-21 19:39:24 +0100

    Last nav bar changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,17 @@
 import React from "react";
 import TopNav from "./Nav";
-import Lists from "./DeafultView";
+import ListChoice from "./ListChoicesTabs"; 
 
 
 const Home = () => {
   return (
     <>
     <TopNav/>
-    <Lists/>
+
+    <div className="p-4 box mt-3 text-center">
+      <h1><b>Your lists</b></h1>
+      <ListChoice/>
+    </div>
       </>
   );
 };
