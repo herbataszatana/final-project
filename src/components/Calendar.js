d9ef3f4330272b3d13aa3be531f2c4013810dfbc
Anna <anma.swiatkowska@gmail.com>
2022-09-02 19:29:32 +0100

    Added firebase init, for deployment and created build

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -8,6 +8,7 @@ const Calendar = () => {
     <TopNav/>
       <div className="p-4 box mt-3 text-center">
       <h1>Calendar</h1>
+      <h2>Sorry this page is still under construction </h2>
       </div>
     </>
   );
